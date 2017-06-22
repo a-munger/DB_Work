235005f67e56e68e57274bf75e737c47abfd131d
a-munger <munger.alexander@gmail.com>
2017-06-22 08:16:02 -0500

    Add files via upload

---
+++ AGP Form/Invoices.js
@@ -0,0 +1,24 @@
+var InvoiceItems = [
+{
+	"CheckDate": "26-OCT-2016",
+	"CheckNumber": "9090192269",
+	"CheckAmount": "1000.00",
+	"CheckType": "Check",
+	"VendorName": "ARP, DAN      M EMP",
+	"AddressLine1": "C/O AG PROCESSING INC",
+	"AddressLine2": "PO BOX 187",
+	"AddressLine3": "",
+	"City": "MANNING",
+	"State": "IA",
+	"Zip": "51455",
+	"Invoices": [
+		{
+			"InvoiceNo": "CKREQ102016",
+			"InvoiceDate": "20-OCT-2016",
+			"Description": "TRAVEL ADVANCE-COURSE TEXAS A&M",
+			"Discount": "0.00",
+			"NetAmount": "1000.00"
+		}
+	]
+}
+];
---
+++ AGP Form/javascript/RequiredDocs.js
@@ -0,0 +1,701 @@
+var ChecklistItems = [
+    {
+      "TransactionType": "Annual Certification",
+      "DocumentTypes": [
+		{
+			"Name": "Annual Recertification Checklist",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Adoption/FosterCare Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Affidavit of No Income Status",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Appointment Letter",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Asset Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Birth Certificate",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Child Care Expense",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Child Support/Alimony Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Client Consent Form for Income Verification and Background Record Release",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Contribution Self-Certification",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Contribution Statement",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Custody/Guardianship Documents",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Debts Owed to Public Housing Agencies and Terminations (HUD-52675)",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Declaration of Section 214 Status",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Did You Know...",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Drug and Alcohol Free Household Statement",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "EIV Printout",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Employment Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Family Composition Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "HUD 50058",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Marital/Separation Documents",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Medical Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Minimum Rent Brochure Acknowledgement of Receipt",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Notice of Change to Lease and Contract",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Participant Recertification Assessment",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Pension/Annuity Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Photo IDs",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Preparation and 100% Documents Quality Control Summary",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Release of Information/Privacy Act Notice (HUD-9886)",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Self-Certification Self-Employment Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "SS Card",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Supplement to application",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Tenant Fraud Letter",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "The Work Number printout",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Third party county verification: DJFS & CSEA",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Third Party Verification Checklist",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "TTP Worksheet",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Veteran's Benefits Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Worker's Compensation Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Zero Income Checklist and Worksheet",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+		}
+      ],
+	  "AdditionalDocuments": [
+		{
+			"Name": "Income Worksheet",
+			"Required": true,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Tax Return Documents",
+			"Required": true,
+			"Met": false,
+			"MetDate": ""
+		}
+	  ]
+    },
+    {
+      "TransactionType": "Intake",
+      "DocumentTypes": [
+{
+			"Name": "Adoption/Foster Care Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Application",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Asset Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Background and Record Release Statement",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Birth Certificate",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Child Care Expense",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Child Support/Alimony Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Client Consent Form for Income Verification and Background Record Release",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Contribution Self-Certification",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Contribution Statement",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "County verification: DJFS & CSEA",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Debts Owed to Public Housing Agencies and Terminations (HUD-52675)",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Declaration of Section 214 Status",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Did You Know...",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Drug and Alcohol Free Household Statement",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "EIV Printout",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Employment Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "File Order Sheet",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Full Time Student Verification",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Important Information Notice",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Income Worksheet",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Marital/Separation Documents",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Medical Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Pension/Annuity Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Photo IDs",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Probate",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Quality Review Checklist ?",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Release of Information/Privacy Act Notice (HUD-9886)",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Rent Worksheet",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Required Verification/Documents",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Self-Certification Self-Employment Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "SS Card",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Supplement to application",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Tenant Fraud Letter",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "The Work Number printout",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Third Party Verification Checklist",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "TTP Worksheet",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Veteran's Benefits Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Worker's Compensation Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+}
+      ],
+	  "AdditionalDocuments": [
+		{
+			"Name": "Tax Worksheet",
+			"Required": true,
+			"Met": false,
+			"MetDate": ""
+		},
+		{
+			"Name": "Income Documents",
+			"Required": true,
+			"Met": false,
+			"MetDate": ""
+		}
+	  ]
+    },
+    {
+      "TransactionType": "Interim Certification",
+      "DocumentTypes": [
+{
+			"Name": "Adoption/Foster Care Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Asset Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Child Care Expense",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Child Support/Alimony Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Client Consent Form for Income Verification and Background Record Release",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Contribution Self-Certification",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Contribution Statement",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Custody/Guardianship Documents",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "EIV Printout",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Employment Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Family Composition Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "HUD 50058",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Interim Acknowledgement",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Interim Recertification Checklist",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Interim Reexaminalion Update Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Marital/Separation Documents",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Medical Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Notice of Change to Lease and Contract",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Pension/Annuity Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Photo IDs",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Release of Information/Privacy Act Notice (HUD-9886)",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Self-Certification Self-Employment Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "SS Card",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "The Work Number printout",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Third party county verification: DJFS & CSEA",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Third Party Verification Checklist",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "TTP Worksheet",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Veteran's Benefits Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+},
+{
+			"Name": "Worker's Compensation Verification Form",
+			"Required": false,
+			"Met": false,
+			"MetDate": ""
+}
+      ],
+	  "AdditionalDocuments": [
+		{
+			"Name": "Insurance Claim Form",
+			"Required": true,
+			"Met": false,
+			"MetDate": ""
+		}
+	  ]
+    }
+  ];
